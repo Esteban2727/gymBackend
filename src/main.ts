3c75682af5ba982082bc7df48075f1e5bc411d2b
Esteban <esteban.castro@contentolatam.com>
2025-03-16 12:07:12 -0500

    cambiando package.json

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ import { existsSync, mkdirSync } from 'fs';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { CONFIG_SESSION } from './configNest/config';
+import * as crypto from 'crypto';
 
+// Asegurar que crypto está disponible globalmente
+if (!global.crypto) {
+  global.crypto = crypto as any;
+}
 declare module 'express-session' {
   interface SessionData {
     user: {
