5e49a88e42e795a3f0f4e1f80b9021af6565fd7b
Jhon Noises <jhonseagle@gmail.com>
2025-04-21 23:53:10 -0500

    cambiando a bycriptjs

--- src/auth/bycript/bycript.ts
+++ src/auth/bycript/bycript.ts
@@ -1,8 +1,9 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 export const comparePasswords = async (password: string, hash: string) => {
   return bcrypt.compare(password, hash);
 };
+
 export const HashearPassword = async (password: string) => {
   const hashedPassword: string = await bcrypt.hash(
     password,
