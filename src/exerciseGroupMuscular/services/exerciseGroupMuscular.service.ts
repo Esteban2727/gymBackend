09465a671264c10206bb6bd36e24df2c4ac348a0
esteban2727 <escastr@gmail.com>
2025-04-22 15:42:10 -0500

    Feat: Add Repository in excerciseGroupMuscular.service - Fiend muscular group on the table

--- src/exerciseGroupMuscular/services/exerciseGroupMuscular.service.ts
+++ src/exerciseGroupMuscular/services/exerciseGroupMuscular.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ExerciseMuscleGroup } from "../exerciseGroupMuscular.entity";
 import { Repository } from "typeorm";
-
+import { MuscleGroup } from "src/groupMuscle/Entity/muscleGroup.entity";
 @Injectable()
 export class ExerciseGroupMuscularServices{
-    constructor() {
-
+    constructor(@InjectRepository(MuscleGroup)readonly groupMusculeRepository:Repository<MuscleGroup>) {
     }
 async getGroupMuscularServices(){
-    return 'respuesta'
+    const getData = await this.groupMusculeRepository.find()
+    return getData
 }
 }
